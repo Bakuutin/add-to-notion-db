22a130f811438277a3f8811edb8172b2d9f487aa
Igor Bakutin <igorbakutin@gmail.com>
2022-07-27 23:58:01 +0400

    upd

--- handler.ts
+++ handler.ts
@@ -1,6 +1,67 @@
-export async function main(event) {
+import { Client } from "@notionhq/client";
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+interface Event {
+  text: string;
+}
+
+const TITLE_LIMIT = 1500
+
+
+async function appendParagraph(pageId: string, text: string) {
+  await notion.blocks.children.append({
+    block_id: pageId,
+    children: [
+      {
+        type: "paragraph",
+        object: "block",
+        paragraph: {
+          rich_text: [
+            {
+              type: "text",
+              text: { "content": text },
+            },
+          ],
+        },
+      },
+    ],
+  })
+}
+
+async function createPage(title: string) {
+  const { id } = await notion.pages.create({
+    parent: { database_id: process.env.NOTION_DATABASE_ID as string },
+    properties: { Name: { title: [{ text: { content: title } }] } },
+  })
+
+  return id
+}
+
+
+export const main: APIGatewayProxyHandler = async event => {
+  let { text }: Event = JSON.parse(event.body || '') || {}
+
+
+  const title = text ? text.slice(0, TITLE_LIMIT) : new Date().toString();
+  const pageId = await createPage(title)
+
+
+  if (text.length > TITLE_LIMIT) {
+    // split paragraph onto blocks of max 1500 characters
+    const paragraphs = text.split('\n').map(paragraph => paragraph.match(/.{1,1500}/g) || ['']).flat()
+
+    for (let paragraph of paragraphs) {
+      await appendParagraph(pageId, paragraph)
+    }
+  }
+
   return {
-    message: 'Go Serverless v3! Your function executed successfully!',
-    input: event,
+    statusCode: 200,
+    body: JSON.stringify({ pageId }),
   };
 }
